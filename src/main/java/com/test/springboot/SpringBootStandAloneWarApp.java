8ac375d3d9f66fadd1bc9e48cff046f34437d703
shailendra333 <shailendra.iitr.111@gmail.com>
2019-03-08 18:32:09 +0530

    first commit
    initial commit

---
+++ src/main/java/com/test/springboot/SpringBootStandAloneWarApp.java
@@ -0,0 +1,22 @@
+package com.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+
+@SpringBootApplication
+public class SpringBootStandAloneWarApp extends SpringBootServletInitializer{
+
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootStandAloneWarApp .class);
+    }
+    
+    public static void main(String[] args) {
+		SpringApplication.run(SpringBootStandAloneWarApp.class, args);
+	}
+}
